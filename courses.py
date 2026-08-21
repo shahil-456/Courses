2f06873841c409ab853564edbfaacd95385eb56b
shahil-git <shahilshadh037@gmail.com>
2026-08-21 21:19:17 +0530

    audio

--- courses.py
+++ courses.py
@@ -41,7 +41,7 @@ for item in data["items"]:
     try:
         course_name = clean_name(item["name"])
         page = item["page"]
-        print('Download Started')
+        print('Download Started---')
         page_folder = os.path.join(
             "my courses",
             course_name,
@@ -101,7 +101,34 @@ print("Errors:", errors)
 
 
 
+def create_audio_files():
+    for root, dirs, files in os.walk("my courses"):
 
+        for file in files:
+
+           if not file.lower().endswith((".mp4", ".mov")):
+                continue
+
+            video_path = os.path.join(root, file)
+            audio_path = os.path.splitext(video_path)[0] + ".mp3"
+
+            if os.path.exists(audio_path):
+                continue
+
+            print("Converting:", video_path)
+
+            subprocess.run([
+                "ffmpeg",
+                "-i", video_path,
+                "-vn",
+                "-q:a", "2",
+                audio_path
+            ], check=True)
+
+            print("Created:", audio_path)
+
+
+create_audio_files()
 
 
 
