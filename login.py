f09f78b98fdb47338c3f52fb82e324c33c8dbae6
shahil-git <shahilshadh037@gmail.com>
2026-09-01 20:45:31 +0530

    login

---
+++ login.py
@@ -0,0 +1,17 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.firefox.launch(headless=False)
+
+    context = browser.new_context(storage_state="state.json")
+    
+    page = context.new_page()
+
+    page.goto("https://www.docmeded.com/video/item/11222")
+
+    input("Login manually, then press Enter here...")
+
+    context.storage_state(path="state.json")
+
+    print("state.json saved")
+    browser.close()
