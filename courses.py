1260dfc99ee1c580250726da64e9c21b453ba898
shahil-git <shahilshadh037@gmail.com>
2026-09-01 20:41:29 +0530

    new fix

--- courses.py
+++ courses.py
@@ -5,103 +5,77 @@ import json
 import time
 import subprocess
 
-# def extract_ts_links(filename="ts_links.yaml"):
-#     with open(filename, "r", encoding="utf-8") as f:
-#         lines = f.readlines()
-
-#     count = 1
-
-#     for line in lines:
-#         if "1080_00001" in line:
-#             ts_url = line.strip()
-
-#             with open(f"ex-{count}.json", "w", encoding="utf-8") as f:
-#                 json.dump({
-#                     "name": f"ex-{count}",
-#                     "link": ts_url
-#                 }, f, indent=4)
-
-#             count += 1
-
-# extract_ts_links()
-
-
-# current_dir = os.getcwd()
-
-# for folder in os.listdir(current_dir):
-#     folder_path = os.path.join(current_dir, folder)
-
-#     if os.path.isdir(folder_path):
-#         files = os.listdir(folder_path)
-
-#         has_json = any(f.lower().endswith(".json") for f in files)
-#         has_key = any(f.lower().endswith(".key") for f in files)
-#         has_m3u8 = any(f.lower().endswith(".m3u8") for f in files)
-
-#         if not (has_json and has_key and has_m3u8):
-#             print(folder)
-
-
-
 
 def download_1080_ts():
 
     current_dir = os.path.join(os.getcwd(), "videos")
+    while True:
+        for folder in os.listdir(current_dir):
 
-    for folder in os.listdir(current_dir):
+            try:
+                folder_path = os.path.join(current_dir, folder)
 
-        try:
-            folder_path = os.path.join(current_dir, folder)
+                if not os.path.isdir(folder_path):
+                    continue
 
-            if not os.path.isdir(folder_path):
+                files = os.listdir(folder_path)
+
+                if not any(f.endswith(".m3u8") for f in files):
+                    continue
+
+                if not any(f.endswith(".ts") for f in files):
+                    continue
+
+                if not any(f.endswith(".key") for f in files):
+                    continue
+
+                json_path = os.path.join(folder_path, "ts.json")
+                print('downloading')
+
+                if not os.path.exists(json_path):
+                    continue
+                
+                with open(json_path, "r", encoding="utf-8") as f:
+                    data = json.load(f)
+
+                if "saved" in data:
+                    continue
+
+                # Get link from ts.json
+                link = data["link"]
+
+                # Replace quality with 1080
+                link = (link.replace("/240_", "/1080_")
+                            .replace("/480_", "/1080_")
+                            .replace("/720_", "/1080_"))
+
+                # Download 1080_00000.ts, 1080_00001.ts, ...
+                index = 0
+
+                while True:
+                    url = link.replace("1080_00000.ts", f"1080_{index:05d}.ts")
+                    output = os.path.join(folder_path, f"1080_{index:05d}.ts")
+                    time.sleep(0.3)
+                    response = requests.get(url)
+
+                    if response.status_code != 200:
+                        break
+
+                    with open(output, "wb") as f:
+                        f.write(response.content)
+
+                    print(f"{folder}: 1080_{index:05d}.ts")
+                    index += 1
+
+                data["saved"] = True
+
+                with open(json_path, "w", encoding="utf-8") as f:
+                    json.dump(data, f, indent=4)
+
+            except Exception as e:
+                print(f"Error in {folder}: {e}")
                 continue
-
-            json_path = os.path.join(folder_path, "ts.json")
-            print('downloading')
-            if not os.path.exists(json_path):
-                continue
-            
-            with open(json_path, "r", encoding="utf-8") as f:
-                data = json.load(f)
-
-            if "saved" in data:
-                continue
-
-            # Get link from ts.json
-            link = data["link"]
-
-            # Replace quality with 1080
-            link = (link.replace("/240_", "/1080_")
-                        .replace("/480_", "/1080_")
-                        .replace("/720_", "/1080_"))
-
-            # Download 1080_00000.ts, 1080_00001.ts, ...
-            index = 0
-
-            while True:
-                url = link.replace("1080_00000.ts", f"1080_{index:05d}.ts")
-                output = os.path.join(folder_path, f"1080_{index:05d}.ts")
-                time.sleep(0.3)
-                response = requests.get(url)
-
-                if response.status_code != 200:
-                    break
-
-                with open(output, "wb") as f:
-                    f.write(response.content)
-
-                print(f"{folder}: 1080_{index:05d}.ts")
-                index += 1
-
-            data["saved"] = True
-
-            with open(json_path, "w", encoding="utf-8") as f:
-                json.dump(data, f, indent=4)
-
-        except Exception as e:
-            print(f"Error in {folder}: {e}")
-            continue
-
+        time.sleep(3)        
 # after download all ts files add key saved :true inside ts.json/
 
 download_1080_ts()
@@ -122,31 +96,31 @@ download_1080_ts()
 
 
 
-current_dir = os.path.join(os.getcwd(), "videos")
+# current_dir = os.path.join(os.getcwd(), "videos")
 
-for folder in os.listdir(current_dir):
-    folder_path = os.path.join(current_dir, folder)
+# for folder in os.listdir(current_dir):
+#     folder_path = os.path.join(current_dir, folder)
 
-    if not os.path.isdir(folder_path):
-        continue
+#     if not os.path.isdir(folder_path):
+#         continue
 
-    if os.path.exists(os.path.join(folder_path, "output.mp4")):
-        continue
+#     if os.path.exists(os.path.join(folder_path, "output.mp4")):
+#         continue
 
-    m3u8 = os.path.join(folder_path, "1080.m3u8")
-    output = os.path.join(folder_path, "output.mp4")
-    time.sleep(0.1)
-    if os.path.exists(m3u8):
-        print(f"Converting: {folder}")
+#     m3u8 = os.path.join(folder_path, "1080.m3u8")
+#     output = os.path.join(folder_path, "output.mp4")
+#     time.sleep(0.1)
+#     if os.path.exists(m3u8):
+#         print(f"Converting: {folder}")
 
-        subprocess.run([
-            "ffmpeg",
-            "-allowed_extensions", "ALL",
-            "-i", "1080.m3u8",
-            "-c", "copy",
-            "output.mp4"
-        ], cwd=folder_path)
+#         subprocess.run([
+#             "ffmpeg",
+#             "-allowed_extensions", "ALL",
+#             "-i", "1080.m3u8",
+#             "-c", "copy",
+#             "output.mp4"
+#         ], cwd=folder_path)
 
-    # break
+#     # break
 
-print("Done")
+# print("Done")
---
+++ one.py
@@ -0,0 +1,165 @@
+from playwright.sync_api import sync_playwright
+import time
+import json
+import os
+import requests
+import re
+
+
+def handle_request(request, title='demo'):
+
+    if ".m3u8" in request.url:
+        print("M3U8:", request.url)
+
+    if ".key" in request.url:
+        print("KEy:", request.url)
+
+    if ".ts" in request.url:
+        # ts_urls.append(request.url)
+        print("TS:", request.url)
+
+
+def save_1080_m3u8(request,title='demo'):
+
+    if ".m3u8" not in request.url:
+        return
+
+    url = request.url.replace("/720.m3u8", "/1080.m3u8")
+    url = url.replace("/480.m3u8", "/1080.m3u8")
+
+    safe_name = re.sub(r'[<>:"/\\|?*]', '', title)
+    folder = os.path.join("videos", safe_name)
+    os.makedirs(folder, exist_ok=True)
+
+    response = requests.get(url)
+
+    if response.ok:
+        with open(os.path.join(folder, "1080.m3u8"), "w", encoding="utf-8") as f:
+            f.write(response.text)
+
+        print("Saved:", url)
+
+
+def save_1080_key(request,title='demo'):
+    if ".key" not in request.url:
+        return
+
+    url = request.url.replace("/720.key", "/1080.key")
+    url = url.replace("/480.key", "/1080.key")
+
+    safe_name = re.sub(r'[<>:"/\\|?*]', '', title)
+    folder = os.path.join("videos", safe_name)
+    os.makedirs(folder, exist_ok=True)
+
+    response = requests.get(url)
+
+    if response.ok:
+        with open(os.path.join(folder, "1080.key"), "wb") as f:
+            f.write(response.content)
+
+        print("Saved key:", url)
+
+def save_1080_ts(request,title='demo'):
+    # ts_saved = False
+
+    # nonlocal ts_saved
+    # if ".ts" not in request.url or ts_saved:
+    #     return
+    if ".ts" not in request.url:
+        return
+
+    url = request.url.replace("/720_", "/1080_")
+    url = url.replace("/480_", "/1080_")
+
+    safe_name = re.sub(r'[<>:"/\\|?*]', '', title)
+    folder = os.path.join("videos", safe_name)
+    os.makedirs(folder, exist_ok=True)
+
+    ts_name = url.split("/")[-1].split("?")[0]
+
+    response = requests.get(url)
+
+    if response.ok:
+        with open(os.path.join(folder, ts_name), "wb") as f:
+            f.write(response.content)
+
+        with open(os.path.join(folder, "ts.json"), "w", encoding="utf-8") as f:
+            json.dump({
+                "name": ts_name,
+                "link": url
+            }, f, indent=4)
+
+        print("Saved TS:", ts_name)
+
+    # ts_saved = True
+
+
+def watch_videos(context, page):
+
+    urls = page.locator(
+        "table.items tbody tr td:first-child a"
+    ).evaluate_all(
+        """els => els.map(e => new URL(e.href, location.origin).href)"""
+    )
+
+    for url in urls:
+        video_page = context.new_page()
+
+        # video_page.on("request", handle_request)
+
+        print(f"Opening: {url}")
+        video_page.goto(url, wait_until="domcontentloaded")
+        video_page.wait_for_timeout(20000)
+
+
+        video_page.goto(url, wait_until="domcontentloaded")
+
+        title = video_page.locator("h1").inner_text()
+
+        video_page.on("request", lambda request: handle_request(request, title))
+        
+        video_page.on("request", lambda request: save_1080_m3u8(request, title))
+        video_page.on("request", lambda request: save_1080_key(request, title))
+        video_page.on("request", lambda request: save_1080_ts(request, title))
+
+        # video_page.on("request", save_1080_m3u8)
+        # video_page.on("request", save_1080_key)
+        # video_page.on("request", save_1080_ts)
+
+        time.sleep(5)
+        video_page.close()
+
+
+
+
+def open_video():
+    with sync_playwright() as p:
+        browser = p.firefox.launch(headless=False)
+
+        context = browser.new_context(
+            storage_state="state.json"
+        )
+
+        page = context.new_page()
+        page.goto(
+            "https://www.docmeded.com/user/watchvideo?userVideoID=550862",
+            wait_until="domcontentloaded"
+        )
+        watch_videos(context, page)
+
+        # Wait for user to login manually if redirected to login
+        if "site/login" in page.url:
+            page.wait_for_url(
+                "**/user/watchvideo?userVideoID=550862",
+                timeout=0
+            )
+
+            context.storage_state(path="state.json")
+            print("state.json updated")
+
+        page.wait_for_timeout(5000)
+
+
+
+
+open_video()
