7932eb28409c70b32bfca032e85edb7ab0c85321
shahil-git <shahilshadh037@gmail.com>
2026-09-11 20:07:17 +0530

    last fix

--- asset.py
+++ asset.py
@@ -317,7 +317,7 @@ def assets():
 
         # print("Asset URL:", asset_url)
 
-        # find_media(data, asset_url, root)
+        find_media(data, asset_url, root)
         # print(root)
         time.sleep(2)
         find_html(data, asset_url, root)
